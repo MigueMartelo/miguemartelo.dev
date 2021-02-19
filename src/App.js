14533622c869e93b53f1309012fd7c4d03858e12
Miguel Martelo <miguemartelo1031@gmail.com>
2021-02-19 18:33:56 -0500

    Add tailwind

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 function App() {
   return (
-    <div>
-      <h1>MigueMartelo.Dev</h1>
+    <div className='font-sans text-center'>
+      <h1 className="text-xl">MigueMartelo.Dev</h1>
     </div>
   );
 }
